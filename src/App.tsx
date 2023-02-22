81394c07e079264794696853fd7729aedb420a95
Kutlu Karakoc <karakockutlu@gmail.com>
2023-02-22 23:32:08 +0300

    added form structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import Form from "./components/Form";
 
 const App: React.FC = () => {
     return (
-        <div className="App">
+        <div className="flex flex-col h-full">
             <h1 className="text-3xl font-bold bg-green-400 text-center p-4">
                 Birthday Reminder
             </h1>
+            <div className="flex justify-around items-center flex-1">
+                <Birthdays />
+                <Form />
+            </div>
         </div>
     );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,11 @@
 const Form: React.FC = () => {
     return (
-        <div>
-            Form
+        <div className="h-60 flex flex-col justify-center items-center gap-6 px-5 border-2 border-black rounded-md">
+            <h6 className="text-sm w-72">Enter the name and date of birth of the person whose birthday you want to remember.</h6>
+            <form action="" className="flex flex-col gap-4">
+                <input type="text" placeholder="Enter name here" className="border border-black rounded-md outline-none p-2"/>
+                <input type="datetime-local" className="border border-black rounded-md outline-none p-2"/>
+            </form>
         </div>
     )
 }
