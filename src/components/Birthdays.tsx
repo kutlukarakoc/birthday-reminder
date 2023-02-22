1071943937dd902e860e08b8dd7ec2088c17b6bb
Kutlu Karakoc <karakockutlu@gmail.com>
2023-02-22 23:45:06 +0300

    added skeleton of birthday list

--- src/components/Birthdays.tsx
+++ src/components/Birthdays.tsx
@@ -1,7 +1,20 @@
 const Birthdays: React.FC = () => {
     return (
-        <div>
-            BirthDays
+        <div className="min-h-[250px] w-96 flex flex-col items-center justify-center gap-6 px-5 border-2 border-black rounded-md">
+            <ul className="w-full h-full flex flex-col items-start gap-4">
+                <li className="w-full flex justify-between px-7">
+                    <div>Person 1</div>
+                    <div>Birthday</div>
+                </li>
+                <li className="w-full flex justify-between px-7">
+                    <div>Person 2</div>
+                    <div>Birthday</div>
+                </li>                
+                <li className="w-full flex justify-between px-7"> 
+                    <div>Person 3</div>
+                    <div>Birthday</div>
+                </li>
+            </ul>
         </div>
     )
 }
