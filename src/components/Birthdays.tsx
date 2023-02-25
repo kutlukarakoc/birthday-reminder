ffb85b8324f708e918a5887a901eec00bd7f778f
Kutlu Karakoc <karakockutlu@gmail.com>
2023-02-25 03:07:06 +0300

    order people depends on days left

--- src/components/Birthdays.tsx
+++ src/components/Birthdays.tsx
@@ -7,7 +7,7 @@ type IBirthdayProps = {
 
 const Birthdays: React.FC<IBirthdayProps> = ({ birthdays, removeBirthday }) => {
 
-    const countDown = (personName: string ,personDay: string, personMonth: string) => {
+    const countDown = (personId: number, personName: string, personDay: string, personMonth: string) => {
         const birthDate: string = personDay
         const birthMonth: string = personMonth
         const today: any = new Date()
@@ -33,13 +33,18 @@ const Birthdays: React.FC<IBirthdayProps> = ({ birthdays, removeBirthday }) => {
         let formattedCountDownDate: string = `${formattedDay}/${formattedMonth}/${nextYear}`
 
         return (
-            <div className={days < 7 ? 'bg-red-400' : 'bg-blue-400'}>
-                <div className="text-lg">{personName}</div>
-                <div>
-                    {days < 10 ? '0' + days : days} days left. <br />
-                    Next birthday: {formattedCountDownDate}
+            <li className="w-full flex items-center" style={{order: days}}>
+                <div className="w-full">
+                    <div className={days < 7 ? 'bg-red-400' : 'bg-blue-400'}>
+                        <div className="text-lg">{personName}</div>
+                        <div>
+                            {days < 10 ? '0' + days : days} days left. <br />
+                            Next birthday: {formattedCountDownDate}
+                        </div>
+                    </div>
+                    <button type="button" className="absolute right-2 top-1 text-lg bg-black text-white rounded-full w-5 h-5 grid place-content-center pb-0.5 z-10" onClick={() => removeBirthday(personId)}>x</button>
                 </div>
-            </div>
+            </li >
         )
     }
 
@@ -48,12 +53,7 @@ const Birthdays: React.FC<IBirthdayProps> = ({ birthdays, removeBirthday }) => {
             <ul className="w-full h-full flex flex-col justify-center items-start gap-4">
                 {
                     birthdays.map(person => (
-                        <li className="w-full flex items-center" key={person.id}>
-                            <div className="w-full text-center relative">
-                                {countDown(person.name, person.day, person.month)}
-                                <button type="button" className="absolute right-2 top-1 text-lg bg-black text-white rounded-full w-5 h-5 grid place-content-center pb-0.5 z-10" onClick={() => removeBirthday(person.id)}>x</button>
-                            </div>
-                        </li>
+                        <div key={person.id} className="w-full text-center relative">{countDown(person.id, person.name, person.day, person.month)}</div>
                     ))
                 }
             </ul>
